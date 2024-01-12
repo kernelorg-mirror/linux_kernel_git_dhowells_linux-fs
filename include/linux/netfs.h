kernelorg-mirror/linux_kernel_git_jlayton_linux

netfs: Decrypt encrypted content
Implement a facility to provide decryption for encrypted content to a whole
read-request in one go (which might have been stitched together from
disparate sources with divisions that don't match page boundaries).

Note that this doesn't necessarily gain the best throughput if the crypto
block size is equal to or less than the size of a page (in which case we
might be better doing it as pages become read), but it will handle crypto
blocks larger than the size of a page.

Signed-off-by: David Howells <dhowells@redhat.com>

include/linux/netfs.h
@@ -207,6 +207,7 @@ struct netfs_read_request {
 #define NETFS_RREQ_DONT_UNLOCK_FOLIOS	3	/* Don't unlock the folios on completion */
 #define NETFS_RREQ_FAILED		4	/* The request failed */
 #define NETFS_RREQ_IN_PROGRESS		5	/* Unlocked when the request completes */
+#define NETFS_RREQ_DECRYPT		6	/* Decrypted when the request completes */
 	const struct netfs_request_ops *netfs_ops;
 };
 
@@ -391,6 +392,9 @@ struct netfs_request_ops {
 	bool (*encrypt_block)(struct netfs_write_request *wreq, loff_t pos, size_t len,
 			      struct scatterlist *source_sg, unsigned int n_source,
 			      struct scatterlist *dest_sg, unsigned int n_dest);
+	int (*decrypt_block)(struct netfs_read_request *rreq, loff_t pos, size_t len,
+			     struct scatterlist *source_sg, unsigned int n_source,
+			     struct scatterlist *dest_sg, unsigned int n_dest);
 };
 
 /*

kernelorg-mirror/linux_kernel_git_dhowells_linux-fs

cc: Jeff Layton <jlayton@kernel.org>
cc: linux-cachefs@redhat.com
cc: linux-fsdevel@vger.kernel.org
cc: linux-mm@kvack.org

include/linux/netfs.h
@@ -330,6 +330,9 @@ struct netfs_request_ops {
 	int (*encrypt_block)(struct netfs_io_request *wreq, loff_t pos, size_t len,
 			     struct scatterlist *source_sg, unsigned int n_source,
 			     struct scatterlist *dest_sg, unsigned int n_dest);
+	int (*decrypt_block)(struct netfs_io_request *rreq, loff_t pos, size_t len,
+			     struct scatterlist *source_sg, unsigned int n_source,
+			     struct scatterlist *dest_sg, unsigned int n_dest);
 };
 
 /*