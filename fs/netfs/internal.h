f5fabf5792967e12b5729c57a0171c72e13b2ab8
David Howells <dhowells@redhat.com>
2024-09-03 17:17:21 +0100

    netfs: Add a content crypto alignment check function
    Add a function to determine if the read/write buffer, the file position and
    the file length conform to the crypto algorithm alignment which would then
    allow us to do crypto directly between that buffer and the bounce buffer,
    thereby saving a copy.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: Jeff Layton <jlayton@kernel.org>
    cc: linux-cachefs@redhat.com
    cc: linux-fsdevel@vger.kernel.org
    cc: linux-mm@kvack.org

--- fs/netfs/internal.h
+++ fs/netfs/internal.h
@@ -383,6 +383,25 @@ static inline bool netfs_are_all_subreqs_queued(const struct netfs_io_request *r
 }
 
 /*
+ * Check to see if a buffer aligns with the crypto block size.  If it doesn't
+ * the crypto layer is going to copy all the data - in which case relying on
+ * the crypto op for a free copy is pointless.
+ */
+static inline bool netfs_is_crypto_aligned(struct netfs_io_request *rreq,
+					   struct iov_iter *iter)
+{
+	unsigned long align;
+	size_t bsize = rreq->crypto_bsize;
+
+	if (!bsize)
+		return true;
+	if ((rreq->start | rreq->len) & (bsize - 1))
+		return false;
+	align = iov_iter_alignment(iter);
+	return (align & (bsize - 1)) == 0;
+}
+
+/*
  * fscache-cache.c
  */
 #ifdef CONFIG_PROC_FS
